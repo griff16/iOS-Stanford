904701944885ca7a8c08a88b79114507a9820e94
AkiraMain <griffcho16@gmail.com>
2022-04-04 20:16:12 +0900

    Initial Commit

---
+++ Concentration_SwiftUI/Concentration_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration_SwiftUIApp.swift
+//  Concentration_SwiftUI
+//
+//  Created by Akira Cho on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct Concentration_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concentration_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concentration_SwiftUI
+//
+//  Created by Akira Cho on 2022/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
