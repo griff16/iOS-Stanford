73138aa7823e78d09317e61ad748b8725cac8d97
AkiraMain <griffcho16@gmail.com>
2022-04-05 13:57:23 +0900

    first commit

--- Concentration_SwiftUI/ContentView.swift
+++ Concentration_SwiftUI/ContentView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello World")
+                .foregroundColor(Color.orange)
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
