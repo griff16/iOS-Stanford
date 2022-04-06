79f5c13c02e69cfe232a94f4beb34e28e1b85f48
Akira Zhang <akira_zhang@apple.com>
2022-04-06 20:59:01 +0900

    finished course 2

--- Concentration_SwiftUI/ContentView.swift
+++ Concentration_SwiftUI/ContentView.swift
@@ -8,32 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["✌","😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳"]
+    @State var emojiCount = 3
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello World")
-                .foregroundColor(Color.orange)
-            Text("Hello World22")
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
     }
 }
 
-
-
-
-
-
-
-
-
-
+struct CardView: View {
+    var content = ""
+    @State var isFaceUp = true
+    
+    var body: some View {
+         ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
---
+++ Concentration_SwiftUI/MemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  MemoryGame.swift
+//  Concentration_SwiftUI
+//
+//  Created by Akira Cho on 2022/04/06.
+//
+
+import Foundation
+
+struct MemoryGame {
+    
+}
